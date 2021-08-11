c908796645a8273c4230e2b381e785e389aeb630
stasy <bernastya@inbox.ru>
2021-08-11 14:50:37 +0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  target: 'static',
+  router: {
+    base: '/next-text-app/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
